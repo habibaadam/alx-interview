63df58c66d66fe51ddaf35168337f9c65014f9b1
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-03-05 22:02:22 +0000

    trial 2

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -1,26 +1,38 @@
 #!/usr/bin/python3
-"""
-Function that generates a pascal's triangle
-"""
+"""This script contains a function that
+generates a pascal triangle"""
 
 
 def pascal_triangle(n):
+    """This function returns a list of lists of initegers
+    representing a Pascal's triangle of n
     """
-    pascal_triangle(n): returns a list of lists of integers representing
-    the Pascal’s triangle of n
-    Returns an empty list if n <= 0
-    """
-
+    rows = []
     if n <= 0:
-        return []
-    ps = [[1]]
-    for h in range(1, n):
-        row = [1]
-        pre = ps[h - 1]
-        for j in range(len(pre)):
-            new = pre[j] + pre[j + 1] if j != len(pre) - 1 else 1
-            row.append(new)
+        return rows
 
-        ps.append(row)
+    for i in range(n):
+        # Check if it's the first row
+        if i == 0:
+            rows.append([1])
+            continue
 
-    return ps
+        # copy the contents of the previous row
+        prev = rows[i - 1].copy()
+
+        # Declare a container for the new row and append 1
+        row = []
+        row.append(1)
+
+        # Loop through the contents of the previous row, adding up
+        # the value at index and value at next index
+        j = 0
+        while j < i - 1:
+            row.append(prev[j] + prev[j + 1])
+            j += 1
+
+        # Append 1 and add to list of rows
+        row.append(1)
+        rows.append(row)
+
+    return rows
