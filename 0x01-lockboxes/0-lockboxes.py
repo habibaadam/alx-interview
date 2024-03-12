503ec27df40c32bf8b1fc3b96d35506f4262a005
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-03-12 22:26:20 +0000

    0. Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+a function that determines if a box can be opened with a list of keys
+stored in boxes containing list of lists
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Function checking if boxes can be unlocked
+    Args:
+    boxes: list of lists
+    Returns: True or False
+    """
+    startpoint = 0
+    unlocked = {}
+
+    for everybox in boxes:
+        if len(everybox) == 0 or startpoint == 0:
+            unlocked[startpoint] = 0
+        for key in everybox:
+            if key < len(boxes) and key != startpoint:
+                unlocked[key] = key
+        if len(unlocked) == len(boxes):
+            return True
+        startpoint += 1
+    return False
