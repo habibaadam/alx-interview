ef1bac727350f31d6406698ca016e2e75fa7926b
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-03-28 13:35:36 +0000

    0. Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,75 +1,40 @@
 #!/usr/bin/python3
-"""
-script that reads stdin line by line and computes metrics:
-
-Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
-<status code> <file size> (
-if the format is not this one, the line must be skipped)
-After every 10 lines and/or a keyboard interruption (CTRL + C),
-print these statistics from the beginning:
-Total file size: File size: <total size>
-where <total size> is the sum of all previous <file size>
-Number of lines by status code:
-possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
-if a status code doesn’t appear or is not an integer,
-don’t print anything for this status code
-format: <status code>: <number>
-status codes should be printed in ascending order
+"""This script reads stdin line by line
+and computes metrics
 """
 
 import sys
+import re
 
+status_code_count = {}
+total_file_size = 0
+num_of_read = 0
 
-def process_input():
+try:
+    for line in sys.stdin:
+        num_of_read += 1
+        r = re.search(
+            '^\\d{1,3}.\\d{1,3}.\\d{1,3}.\\d{1,3}\\s-\\s\\[[\\d -:.]*\
+\\]\\s"GET\\s\\/projects\\/260\\sHTTP\\/1.1"\\s\\d{1,3}\\s\\d{1,4}$',
+            line)
+        if r:
+            status = re.search("(?<=1.1\" )\\d{1,3}", line)
+            file_size = re.search("\\d{1,4}$", line)
+            if status_code_count.get(status.group()):
+                status_code_count[status.group()] = status_code_count.get(
+                    status.group()) + 1
+            else:
+                status_code_count[status.group()] = 1
+            total_file_size = total_file_size + int(file_size.group())
+        else:
+            continue
 
-    lines_read = 0
-    possible_stat_codes = {
-        200: 0,
-        301: 0,
-        400: 0,
-        401: 0,
-        403: 0,
-        404: 0,
-        405: 0,
-        500: 0
-    }
-    total_size = 0
+        if num_of_read % 10 == 0:
+            print(f"File size: {total_file_size}")
+            for status in sorted(status_code_count):
+                print(f"{status}: {status_code_count[status]}")
 
-    try:
-        # Read input from stdin line by line
-        for line in sys.stdin:
-            line = line.split(" ")  # split each line according to spaces
-            if len(line) > 4:
-                status_code = int(line[-2])
-
-                # checking existence of status code in possible status code
-                # increment the count of that status code
-                if status_code in possible_stat_codes.keys():
-                    possible_stat_codes[status_code] += 1
-
-                file_size = int(line[-1])
-                total_size += file_size
-
-                # increment or count number of lines read
-                lines_read += 1
-
-            if lines_read == 10:
-                lines_read = 0
-                print(f"File size: {total_size}")
-
-                for key, value in possible_stat_codes.items():
-                    if value != 0:
-                        print('{}: {}'.format(key, value))
-
-    except KeyboardInterrupt:
-        # Handle keyboard interruption (CTRL + C)
-        pass
-
-    finally:
-        for key, value in possible_stat_codes.items():
-            if value != 0:
-                print('{}: {}'.format(key, value))
-
-
-if __name__ == "__main__":
-    process_input()
+finally:
+    print(f"File size: {total_file_size}")
+    for status in sorted(status_code_count):
+        print(f"{status}: {status_code_count[status]}")
