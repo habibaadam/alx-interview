f34c8d5a491f2e389500921779f63b0df0c1e16f
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-03-28 13:45:38 +0000

    log parcing trial

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,18 +1,33 @@
 #!/usr/bin/python3
-"""This script reads stdin line by line
-and computes metrics
+"""
+a script that reads stdin line by line and computes metrics:
+
+Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
+<status code> <file size>
+(if the format is not this one, the line must be skipped)
+After every 10 lines and/or a keyboard interruption (CTRL + C),
+print these statistics from the beginning:
+Total file size: File size: <total size>
+where <total size> is the sum of all previous <file size>
+Number of lines by status code:
+possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
+if a status code doesn’t appear or is not an integer,
+don’t print anything for this status code
+format: <status code>: <number>
+status codes should be printed in ascending order
 """
 
 import sys
 import re
 
+lines_read = 0
 status_code_count = {}
-total_file_size = 0
-num_of_read = 0
+total_size = 0
+
 
 try:
     for line in sys.stdin:
-        num_of_read += 1
+        lines_read += 1
         r = re.search(
             '^\\d{1,3}.\\d{1,3}.\\d{1,3}.\\d{1,3}\\s-\\s\\[[\\d -:.]*\
 \\]\\s"GET\\s\\/projects\\/260\\sHTTP\\/1.1"\\s\\d{1,3}\\s\\d{1,4}$',
@@ -25,16 +40,16 @@ try:
                     status.group()) + 1
             else:
                 status_code_count[status.group()] = 1
-            total_file_size = total_file_size + int(file_size.group())
+            total_size = total_size + int(file_size.group())
         else:
             continue
 
-        if num_of_read % 10 == 0:
-            print(f"File size: {total_file_size}")
+        if lines_read % 10 == 0:
+            print(f"File size: {total_size}")
             for status in sorted(status_code_count):
                 print(f"{status}: {status_code_count[status]}")
 
 finally:
-    print(f"File size: {total_file_size}")
+    print(f"File size: {total_size}")
     for status in sorted(status_code_count):
         print(f"{status}: {status_code_count[status]}")
